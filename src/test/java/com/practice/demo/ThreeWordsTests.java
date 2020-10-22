000812b162aa714f6325498809079cbc5b0f1dad
Dean Agan <agandfr@gmail.com>
2020-10-22 19:32:35 +1100

    Removed unused imports

--- src/test/java/com/practice/demo/ThreeWordsTests.java
+++ src/test/java/com/practice/demo/ThreeWordsTests.java
@@ -1,13 +1,9 @@
 package com.practice.demo;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
